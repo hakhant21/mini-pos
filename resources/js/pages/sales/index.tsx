7d2c3e0870710e95be841228933f1f04db18236a
Htet Aung Khant <hakhant21@gmail.com>
2026-07-13 15:03:24 +0630

    mobile size cart ui update

--- resources/js/pages/sales/index.tsx
+++ resources/js/pages/sales/index.tsx
@@ -100,7 +100,7 @@ export default function SalesIndex({ sales: salesData, summary }: Props) {
                     </Link>
                 </div>
 
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between pb-2">
                             <CardTitle className="text-sm font-medium">
