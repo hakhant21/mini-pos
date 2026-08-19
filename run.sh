9a2e7488eae039f9b4f6d3caa76ca653a4195d65
Htet Aung Khant <hakhant21@gmail.com>
2026-08-20 01:10:08 +0630

    docker added with caddy https://pos.local

--- run.sh
+++ run.sh
@@ -66,6 +66,6 @@ mkcert -key-file docker/caddy/certs/key.pem -cert-file docker/caddy/certs/cert.p
 
 # ── Docker Compose up ───────────────────────────────────
 info "Starting Docker containers..."
-docker compose up -d --verbose
+docker compose up -d --logs app
 
 info "Done! App is up and running at https://pos.local"
