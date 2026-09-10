706af9fc39ba63f3c0ca748e6738b87bb09c43e6
Htet Aung Khant <hakhant21@gmail.com>
2026-09-10 18:48:18 +0630

    update user to choose php-fpm or octane option

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-mkdir -p storage/app/public storage/framework/cache storage/framework/data storage/framework/sessions \
-    storage/framework/testing storage/framework/views bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
-
-exec "$@"
